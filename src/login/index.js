64cb8c033f018487fa5431f86b249d273a7482b1
Mohamed Ismat <mohamedismat91@gmail.com>
2017-09-21 15:12:04 +0300

    Added the login form

--- src/login/index.js
+++ src/login/index.js
@@ -1,5 +1,88 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { reduxForm, Field } from 'redux-form'
+import { connect } from 'react-redux'
+import { Link } from 'react-router'
 
-class Login extends Component {}
+import Messages from '../notifications/Messages'
+import Errors from '../notifications/Errors'
 
-export default Login
+import loginRequest from './actions'
+
+class Login extends Component {
+  static propTypes = {
+    handleSubmit: PropTypes.func,
+    loginRequest: PropTypes.func,
+    login: PropTypes.shape({
+      requesting: PropTypes.bool,
+      successful: PropTypes.bool,
+      messages: PropTypes.array,
+      errors: PropTypes.array,
+    }),
+  }
+
+  submit = (values) => {
+    this.props.loginRequest(values)
+  }
+
+  render () {
+    const {
+      handleSubmit,
+      login: {
+        requesting,
+        successful,
+        messages,
+        errors,
+      },
+    } = this.props
+
+    return (
+      <div className="login">
+        <form className="widget-form" onSubmit={handleSubmit(this.submit)}>
+          <h1>LOGIN</h1>
+          <label htmlFor="email">Email</label>
+          <Field
+            name="email"
+            type="text"
+            id="email"
+            className="email"
+            component="input"
+          />
+          <label htmlFor="password">Password</label>
+          <Field
+            name="password"
+            type="password"
+            id="password"
+            className="password"
+            component="input"
+          />
+          <button action="submit">LOGIN</button>
+        </form>
+        <div className="auth-messages">
+          {!requesting && !!errors.length && (
+            <Errors message="Failure to login due to:" errors={errors} />
+          )}
+          {!requesting && !!messages.length && (
+            <Messages messages={messages} />
+          )}
+          {requesting && <div>Logging in...</div>}
+          {!requesting && !successful && (
+            <Link to="/signup">Need to Signup? Click Here »</Link>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  login: state.login,
+})
+
+const connected = connect(mapStateToProps, { loginRequest })(Login)
+
+const formed = reduxForm({
+  form: 'login',
+})(connected)
+
+export default formed
